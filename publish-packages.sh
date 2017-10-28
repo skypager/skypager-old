95b0ae926a95ac00b018d8143b01e4393c03ada5
soederpop <jon@dais.com>
2017-10-28 03:41:03 -0500

    37.6.1

--- publish-packages.sh
+++ publish-packages.sh
@@ -1,9 +1,29 @@
 main() {
-  lerna publish --skip-npm --yes --repo-version=37.6.0
-  sky run each-changed-package buildPackage
-  ./each-changed-package.sh
+  bin/run skypager-core buildPackage
+  bin/run skypager-docs buildPackage
+  bin/run skypager-runtime buildPackage
+  bin/run skypage buildPackage
+  bin/run skypager-helpers-bundler buildPackage
+  bin/run skypager-helpers-client buildPackage
+  bin/run skypager-helpers-command buildPackage
+  bin/run skypager-helpers-context buildPackage
+  bin/run skypager-helpers-document-type buildPackage
+  bin/run skypager-helpers-document buildPackage
+  bin/run skypager-helpers-page buildPackage
+  bin/run skypager-helpers-repl buildPackage
+  bin/run skypager-helpers-server buildPackage
+  bin/run skypager-helpers-service buildPackage
+  bin/run skypager-helpers-webpack buildPackage
+  bin/run skypager-runtimes-development buildPackage
+  bin/run skypager-runtimes-electron buildPackage
+  bin/run skypager-runtimes-node buildPackage
+  bin/run skypager-runtimes-react buildPackage
+  bin/run skypager-runtimes-universal buildPackage
+  bin/run skypager-runtimes-web buildPackage
+  bin/run skypager-features-document-database buildPackage
+  bin/run skypager-features-file-manager buildPackage
+  bin/run skypager-servers-portfolio buildPackage
   bin/export
-  cd /Users/jon/Projects/skypager/packages/skypager && npm publish
   cd /Users/jon/Projects/skypager/packages/skypager-core && npm publish
   cd /Users/jon/Projects/skypager/packages/skypager-runtime && npm publish
   cd /Users/jon/Projects/skypager/packages/skypage && npm publish
@@ -27,6 +47,7 @@ main() {
   cd /Users/jon/Projects/skypager/packages/skypager-features-document-database && npm publish
   cd /Users/jon/Projects/skypager/packages/skypager-features-file-manager && npm publish
   cd /Users/jon/Projects/skypager/packages/skypager-servers-portfolio && npm publish
+  cd /Users/jon/Projects/skypager/packages/skypager && npm publish
   }
 
   main
