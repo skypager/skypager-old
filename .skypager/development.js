917496ab88ba6c867121435a37d189aeb203f1c2
soederpop <jon@dais.com>
2017-11-15 22:51:17 -0600

    Development infrastructure for desktop app

--- .skypager/development.js
+++ .skypager/development.js
@@ -1,67 +1,13 @@
-/*
-/*
-skypager.mainCompiler = skypager
-  .webpack({
-    target: 'node',
-    libraryTarget: 'umd'
-  })
+async function discoverLocalWebpackModules() {
+  const { skywalker } = skypager
 
-skypager.mainCompiler.define('__PACKAGE__', {  })
-skypager.mainCompiler.define('__BUILD_STATUS__', {  })
-*/
+  await skywalker.projectWalker('src').run()
 
-/*
-try {
-  const bsThemes = require.resolve("skypager-features-bootstrap-themes")
-  skypager.features.register("bootstrap-themes", () => require(bsThemes))
-  skypager.feature("bootstrap-themes").enable()
-} catch (error) {
-  skypager.bsError = error
-}
-*/
+  const requireContext = skywalker.requireContext('src/webpacks/*', { glob: true })
 
-/*
-skypager.websiteCompiler = (o = {}) => {
-  const {
-    appName = skypager.get("argv.appName", skypager.gitInfo.sha),
-    app = skypager.get("argv.app", "src/website.js")
-  } = o
-
-  delete o.appName
-  delete o.app
-
-  const compiler = skypager.webpack(
-    "website",
-    skypager.lodash.defaults({}, o, {
-      target: "web",
-      cwd: skypager.cwd,
-      outputPath: skypager.join("public"),
-      entry: {
-        [appName]: [
-          //'webpack-hot-middleware/client?publicPath=/',
-          app
-        ]
-      }
-    })
-  )
-
-  compiler.rule("babel", {
-    include: [skypager.join("src")],
-    exclude: [skypager.join("node_modules")],
-    loader: "babel-loader",
-    options: { presets: ["skypager"] },
-    test: [skypager.join("src"), f => f.match(/.js/)]
-  })
-
-  return compiler
+  skypager.hide('localWebpackModules', requireContext)
 }
 
-if (skypager.isElectron) {
-  const code = skypager.fsx.readFileSync(skypager.resolve(__dirname, "electron.js")).toString()
-  Promise.resolve(skypager.createCodeRunner(code, { thisContext: true }, skypager.sandbox))
-}
-
-skypager.use(
-	 require(skypager.resolve('packages','skypager-servers-portfolio'))
-)
-*/
+discoverLocalWebpackModules().catch(error => {
+  runtime.setState({ discoverLocalWebpackModulesError: error })
+})
