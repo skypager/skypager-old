1e6e00bbdcb02de29944a24e94c0c73a5a3287a6
soederpop <jon@dais.com>
2017-12-12 22:10:20 -0600

    Implementing serve command for webpack helper

--- src/helpers/webpack/commands/webpack.js
+++ src/helpers/webpack/commands/webpack.js
@@ -99,7 +99,32 @@ export async function watchCompiler(compiler) {
   })
 }
 
-export async function serveCompiler(compiler) {}
+export async function serveCompiler(compiler) {
+  const { port = 3000, host = '0.0.0.0' } = this.runtime.argv
+
+  const devServer = await compiler.createDevServer({
+    historyApiFallback: true,
+    noInfo: false,
+    hot: true,
+    contentBase: skypager.join('public'),
+    port,
+    host,
+    publicPath: compiler.publicPath || '/',
+    quiet: false,
+    ...this.runtime.argv,
+    stats: { colors: true, minimal: true },
+  })
+
+  devServer.listen(port, hostname, err => {
+    if (err) {
+      print(`ERROR Starting Dev Server`)
+      print(err.message, 4)
+      return
+    }
+
+    print(`Dev Server is listening on http://${host}:${port}`)
+  })
+}
 
 export async function useAnonymousCompiler({ action, options, args, argv }) {
   const { runtime } = this
@@ -123,106 +148,4 @@ export function getCompilerAction({ argv, args, availableWebpacks }) {
   }
 
   return { compilerId, action }
-}
-
-export async function oldRun(options = {}) {
-  const { print, clear } = this.cli
-
-  if (this.runtime.argv.noClear || this.runtime.argv.clear === false) {
-  } else {
-    clear()
-  }
-
-  const [
-    action = 'build',
-    compilerId = options.compilerId ||
-      this.runtime
-        .at(
-          'options.projectType',
-          'options.compilerId',
-          'currentPackage.projectType',
-          'currentPackage.name'
-        )
-        .find(v => v && v.length),
-  ] = options._.slice(1)
-
-  options = this.lodash.defaults({}, options, {
-    target: this.runtime.get(
-      'options.defaultBuildTarget',
-      this.runtime.get('currentPackage.defaultBuildTarget', 'node')
-    ),
-    libraryTarget: this.runtime.get(
-      'options.defaultLibraryTarget',
-      this.runtime.get('currentPackage.libraryTarget', 'umd')
-    ),
-  })
-
-  if (options.outputPath) {
-    options.outputPath = this.runtime.resolve(options.outputPath)
-  }
-
-  const isAvailable = compilerId && this.runtime.webpacks.available.indexOf(compilerId) !== -1
-
-  const compilerArgs = []
-
-  if (isAvailable) {
-    compilerArgs.push(...[compilerId, options])
-  } else if (compilerId) {
-    compilerArgs.push(...[compilerId, options])
-  } else {
-    compilerArgs.push(options)
-  }
-
-  console.log('Creating Compiler', isAvailable, compilerArgs)
-
-  const compiler = await this.runtime.compiler(...compilerArgs)
-
-  const doWatch = async (watchOptions = {}) => {
-    compiler.watch(watchOptions)
-    return compiler
-  }
-
-  const doServe = async (devServerOptions = {}) => {
-    const server = await compiler.createDevServer(devServerOptions)
-
-    const listener = new Promise((resolve, reject) => {
-      server.listen(port, host, e => (e ? reject(e) : resolve(server)))
-    })
-
-    return await Promise.resolve(listener)
-  }
-
-  const doBuild = async (runOptions = {}) => {
-    const results = await compiler.run(runOptions).catch(e => e)
-
-    if (results && (!results.stack && !results.message)) {
-      print(compiler.stringifyStats({ colors: true }), 4)
-    } else {
-      print('Compiler failed')
-      console.log(results)
-    }
-
-    return compiler
-  }
-
-  const fatal = (stageDesc, err, c) => {
-    this.runtime.error(`Fatal error encountered while ${stageDesc}`, { message: err.message })
-    return c
-  }
-
-  if (!compiler.isConfigValid) {
-    print('The compiler configuration is invalid')
-    print(compiler.configValidationMessages)
-    return this
-  }
-
-  switch (action) {
-    case 'watch':
-      return doWatch().catch(e => fatal('watching', e, compiler))
-    case 'serve':
-      return doServe().catch(e => fatal('serving', e, compiler))
-    case 'build':
-    default:
-      return doBuild().catch(e => fatal('building', e, compiler))
-  }
 }
