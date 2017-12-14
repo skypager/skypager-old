d6d95e6b85ad0f74ddcf8cca23b6b55eb66dab7c
soederpop <jon@dais.com>
2017-12-14 14:40:40 -0600

    Making the standard project type available by default

--- src/project-types/skypager-project-types-standard/index.js
+++ src/project-types/skypager-project-types-standard/index.js
@@ -3,6 +3,6 @@ const projectType = require('./lib')
 module.exports = Object.assign(projectType, {
   initializer: function(next) {
     const runtime = this
-    runtime.projectTypes.register('semantic-react', () => projectType)
+    runtime.projectTypes.register('standard', () => projectType)
   }
 })
