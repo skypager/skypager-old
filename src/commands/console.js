850fd518093919303118f83a8d4a7d9ff5e59c30
soederpop <jon@dais.com>
2017-12-14 15:29:50 -0600

    Bugfix for console command

--- src/commands/console.js
+++ src/commands/console.js
@@ -15,6 +15,8 @@ export async function prepare() {
   if (!this.runtime.repl) {
     this.runtime.use(require('skypager-helpers-repl'), 'INITIALIZING')
   }
+
+  return true
 }
 
 export async function run() {
