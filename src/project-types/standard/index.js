0442c62a8f2e83ce5b5de449febfda66bae6e3d3
soederpop <jon@dais.com>
2017-12-16 23:49:03 -0600

    Syncing project type versions

--- src/project-types/standard/index.js
+++ src/project-types/standard/index.js
@@ -1,6 +1,7 @@
 const projectType = require('./lib')
 
 module.exports = Object.assign(projectType, {
+  version: '39.4.0',
   initializer: function(next) {
     const runtime = this
     runtime.projectTypes.register('standard', () => projectType)
