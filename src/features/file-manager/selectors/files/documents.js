3f7d883bf729f1f671572e39500f39ccf7df3f8f
soederpop <jon@dais.com>
2018-03-14 01:07:10 -0500

    Make the files/document selector accept a lazy option, to lazy load the document

--- src/features/file-manager/selectors/files/documents.js
+++ src/features/file-manager/selectors/files/documents.js
@@ -3,18 +3,33 @@ export default async function selectDocuments(chain, options = {}) {
   const results = await this.select('files/asts', { ...options, debug: true })
   const { fileIds } = results
 
-  return chain
-    .plant(fileIds)
-    .map(fileId => this.convertToJS(this.fileManager.file(fileId)))
-    .keyBy(file => file.relative)
-    .mapKeys(mapKeys)
-    .mapValues(mapValues)
-    .mapValues((attributes, docId) => {
-      this.documents.register(docId, {
-        ...attributes,
-        id: docId,
-      })
+  return (
+    chain
+      .plant(fileIds)
+      // .map(fileId => this.convertToJS(this.fileManager.file(fileId)))
+      .keyBy(file => file.relative)
+      .mapKeys(mapKeys)
+      .mapValues(mapValues)
+      .mapValues((attributes, docId) => {
+        if (options.lazy) {
+          return (extensions = {}) => {
+            this.documents.register(docId, {
+              ...attributes,
+              id: docId,
+              ...extensions,
+            })
 
-      return this.document(docId)
-    })
+            return this.document(docId)
+          }
+        } else {
+          this.documents.register(docId, {
+            ...attributes,
+            id: docId,
+            ...extensions,
+          })
+
+          return this.document(docId)
+        }
+      })
+  )
 }
